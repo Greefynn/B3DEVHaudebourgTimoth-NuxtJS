b0fc41eb1867e5105b242c1fcbaef3aa7f41f18a
Greefynn <natsucrafteur180@gmail.com>
2021-10-08 16:29:45 +0200

    init but all

---
+++ pages/inspire.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/yB.gif"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, bake the paté de crabe. Then, get a hit from the bong with Patrick.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;SpongeBob Squarepants</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
